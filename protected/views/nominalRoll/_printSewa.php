5502db8dda32aa0da3baf753a0411f6ff680e2ea
anil kumar <anilkumar.dhiman1@gmail.com>
2014-08-26 09:08:04 +0530

    print page

--- protected/views/nominalRoll/_printSewa.php
+++ protected/views/nominalRoll/_printSewa.php
@@ -1,62 +1,218 @@
-<table border="1" width="100%">
-    <tr>
-        <td colspan="8">
-            <table width="100%" border="1">
-                <tr>
-                    <td align="center" colspan="8">RADHA SOAMI SATSANG BEAS</td>
-                </tr>
-                <tr>
-                    <td align="center" colspan="8">NOMINAL ROLL</td>
-                </tr>
-                <tr>
-                    <td align="left" colspan="2">SR. No.:<?php echo $NominalRoll->serial_no; ?></td>
-                    <td align="center" colspan="4">CENTRE:<?php echo $NominalRoll->centre_name; ?></td>
-                    <td align="right" colspan="2">24x7 HELPLINE NO.</td>
-                </tr>
-                <tr>
-                    <td align="left" colspan="2">DATED:<?php echo $NominalRoll->dated; ?></td>
-                    <td align="center" colspan="4">AREA:<?php echo $NominalRoll->area_name; ?>, ZONE-<?php echo $NominalRoll->zone_name; ?></td>
-                    <td align="right" colspan="2"><?php echo $NominalRoll->help_line_no; ?></td>
-                </tr>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta charset="UTF-8">
+<title>Radha Soami Satsang Beas </title>
+<style>
+*{font-family:Arial, Helvetica, sans-serif; line-height:25px; margin:0 auto; font-size:14px; 	    }
+table td {border:1px solid #666;   }
+th{border:1px solid #666; border-collapse:separate; padding:5px 6px; line-height:25px; }
+.wraper{width:750px;}
+table #zonal 
+{
+    border-collapse:separate;
+    border-spacing:0 58px;
+}
+</style>
+</head>
+<body>
+<div class="wraper">
+<table cellspacing="0" cellpadding="0" width="682"  >
+  <tr>
+    <th>RADHA SOAMI SATSANG BEAS</th>
+  </tr>
+  <tr>
+    <th> <u> NOMINAL ROLL</u><br />CENTER : BALTANA<br />AREA : MOHALI, ZONE - IB</th></tr>
+  <tr>
+    <td>
+       <div style="float:left; padding:0 10px; margin:0;">Vehicle: HP 123253 <span style="margin:0; padding-left:10px;">Type : (Truck) Tata 407</span><br>
+       Period From : 16/08/2014 to 16/08/2014 </div>
+       <div style="float:right; margin:0 10px ;">Driver : Narinder Singh (9878904020) <br />
+         Destination : Mohali Sewa
+       </div>
+</td></tr>
+  <tr><td>
+    <table border="0" cellspacing="0" cellpadding="0">
+   
+      <th >S. No.</th>
+      <th >Name of<br />
+Sewadar/Sewadarni</th>
+      <th>Father/Husband Name</th>
+      <th >Sex</th>
+      <th>Age</th>
+      <th>Center<br />
+Token No.</th>
+      <th >Address</th>
+      <th>Contact No.</th>
 
-                <tr>
-                    <td>Vehicle No.:<?php //echo $NominalRoll->?></td>
-                    <td>Vehicle No.:  PB 65V3645            Type:   Bus               Driver: Balwinder Singh(9914879286)</td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-    <tr>
-        <th>Serial No</th>
-        <th>Name of <br/>Sewadar/<br/>Sewadarni</th>
-        <th>Father/ <br/>Husband Name</th>
-        <th>Sex</th>
-        <th>Age</th>
-        <th>Centre <br/>Token No.</th>
-        <th>address</th>
-        <th>Contact No.</th>
-    </tr>
-    <?php
-    $counter = 1;
-    foreach($NominalRollDetail as $list) {
-
-        $data = Sewadars::model()->findByPk($list->sewadar_id);
-        $datetime1 = new DateTime($data->date_of_birth);
-        $datetime2 = new DateTime(date('Y-m-d'));
-        $interval = $datetime1->diff($datetime2);
-        $age =  $interval->format('%y');
-        ?>
         <tr>
-            <td align="center"><?php echo $counter; ?></td>
-            <td><?php echo $data->sewadar_name; ?></td>
-            <td><?php echo $data->father_dauther_son_wife_of; ?></td>
-            <td align="center"><?php echo ($data->gender == 'MALE') ? 'M' : 'F'; ?></td>
-            <td align="center"><?php echo $age; ?></td>
-            <td></td>
-            <td align="center"><?php echo $data->address1.' '.$data->address2.' '. $data->address3; ?></td>
-            <td align="center"><?php echo $data->mobile_primary; ?></td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr><tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr><tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr><tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr><tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr><tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr><tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr><tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr><tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr><tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
         </tr>
-    <?php $counter++;
-    }
-    ?>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr><tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr><tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr
+      
+      
+    </table></td>
+  </tr>
+</table><br>
+<table width="682" cellpadding="0" cellspacing="0">
+  <tr>
+    <td>
+    <section style="float:left; padding:10px 0 5px 10px; ">
+    Name of Incharge : Narender Kumar<br>
+	     Badge No. :<br>
+	     Mobile No. : 9878904040<br>
+         Signature:
+         </section>
+  <section style="float:right; padding:10px 10px 5px 0;">
+	For Radha Soami Satsang Beas <br>
+    Secretary Baltana Centre<br>
+    Secretary/President<br>
+    Contact Nos. 98789-03823, 98558-24925
+    </section></td></tr>
+  </table>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p><br>
+  
+</p>
+<table width="682" cellpadding="0" cellspacing="0">
+  <tr><td>
+  
+<u><strong><center>For Area/Zonal Office Use</center></strong></u>
+<section style="float:left; padding:10px 0px 5px 10px;" >
+	Sr. No. : <?php echo $NominalRoll->serial_no; ?><br>
+	DATED: 16/08/2014<br>
+	No. Of Male Sewadars:<br>
+	Total Sewadars:
+</section>
+  <section style="float:right; padding:10px 10px 5px 0px;"  >
+    Area Secretary Contact Nos. 9779457500 <br>
+     Baltana Centre, Mohali Area, Sone- IB<br>
+     Department Name : Special Sewa<br>
+     Period From: 16/08/2014 to16/08/2014<br>
+<br>
+Incharge Signature
+  </section>
+
+  </td>
+  
+  </tr>
 </table>
+</div>
+</body>
+</html>
