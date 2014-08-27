41e611713c924e12f1ef4b635d7db89eb5b6c4b9
anil kumar <anilkumar.dhiman1@gmail.com>
2014-08-27 09:51:56 +0530

    print page change and nomal roll page changes

--- protected/views/nominalRoll/_printSewa.php
+++ protected/views/nominalRoll/_printSewa.php
@@ -1,218 +1,133 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta charset="UTF-8">
-<title>Radha Soami Satsang Beas </title>
-<style>
-*{font-family:Arial, Helvetica, sans-serif; line-height:25px; margin:0 auto; font-size:14px; 	    }
-table td {border:1px solid #666;   }
-th{border:1px solid #666; border-collapse:separate; padding:5px 6px; line-height:25px; }
-.wraper{width:750px;}
-table #zonal 
-{
-    border-collapse:separate;
-    border-spacing:0 58px;
-}
-</style>
+    <meta charset="UTF-8">
+    <title>Radha Soami Satsang Beas </title>
+    <style>
+        *{font-family:Arial, Helvetica, sans-serif; line-height:20px; margin:0 auto; font-size:13px; 	    }
+        table td {border:1px solid #CCC; line-height:18px;  }
+        th{border:1px solid #ccc; border-collapse:separate; padding:5px 6px; line-height:13px;   }
+        .wraper{width:750px;}
+        table #zonal
+        {
+            border:none;
+            text-align:center;
+            line-height:22px;
+        }
+    </style>
 </head>
 <body>
 <div class="wraper">
 <table cellspacing="0" cellpadding="0" width="682"  >
-  <tr>
+<tr>
     <th>RADHA SOAMI SATSANG BEAS</th>
-  </tr>
-  <tr>
-    <th> <u> NOMINAL ROLL</u><br />CENTER : BALTANA<br />AREA : MOHALI, ZONE - IB</th></tr>
-  <tr>
-    <td>
-       <div style="float:left; padding:0 10px; margin:0;">Vehicle: HP 123253 <span style="margin:0; padding-left:10px;">Type : (Truck) Tata 407</span><br>
-       Period From : 16/08/2014 to 16/08/2014 </div>
-       <div style="float:right; margin:0 10px ;">Driver : Narinder Singh (9878904020) <br />
-         Destination : Mohali Sewa
-       </div>
-</td></tr>
-  <tr><td>
-    <table border="0" cellspacing="0" cellpadding="0">
-   
-      <th >S. No.</th>
-      <th >Name of<br />
-Sewadar/Sewadarni</th>
-      <th>Father/Husband Name</th>
-      <th >Sex</th>
-      <th>Age</th>
-      <th>Center<br />
-Token No.</th>
-      <th >Address</th>
-      <th>Contact No.</th>
+</tr>
+<tr>
+    <th>
+        <div style="float:left; margin:18px 0 0px 5px; text-align:left; line-height:20px;">
+            SR.NO.: <?php echo $NominalRoll->serial_no; ?><br>DATED: <?php echo date('d/m/Y',strtotime($NominalRoll->dated)); ?>
+        </div>
 
+        <div style=" position:relative; display:block;  float:left; text-align:center; line-height:13px; left:128px; ">
+            NOMINAL ROLL</u><br />CENTER : <?php echo $NominalRoll->centre_name; ?><br />AREA : <?php echo $NominalRoll->area_name; ?>, ZONE - <?php echo $NominalRoll->zone_name; ?> </div>
+
+        <div style="float:right; margin:18px 5px 0px 0; text-align:right; line-height:20px; ">
+            24X7 HELPLINE NO.<br><?php echo $NominalRoll->help_line_no; ?>
+        </div> </th></tr>
+<tr>
+    <td valign="top">
+        <div style="float:left; padding:0 10px; margin:0; ">Vehicle: <?php echo $NominalRoll->driver_vehicle_no; ?> <span style="margin:0; padding-left:50px;">Type : <?php echo $NominalRoll->driver_vehicle_type; ?></span><br>
+            Period From : <?php echo date('d/m/Y', strtotime($NominalRoll->period_from)); ?> to <?php echo date('d/m/Y',strtotime($NominalRoll->period_to)); ?> </div>
+        <div style="float:right; margin:0 10px 4px;">Driver : <?php echo $NominalRoll->drive_name; ?> (<?php echo $NominalRoll->drive_mobile_no; ?>) <br />
+            Destination : <?php echo $NominalRoll->destination; ?>
+        </div>
+    </td></tr>
+<tr><td>
+<table cellspacing="0" cellpadding="0" id="zonal" width="100%">
+
+<th align="center" >S. No.</th>
+<th >Name of<br />
+    Sewadar/Sewadarni</th>
+<th>Father/Husband Name</th>
+<th >Sex</th>
+<th>Age</th>
+<th>Center<br />
+    Token No.</th>
+<th >Address</th>
+<th>Contact No.</th>
+
+    <?php
+    $counter = 1;
+    foreach($NominalRollDetail as $list) {
+
+        $data = Sewadars::model()->findByPk($list->sewadar_id);
+        $datetime1 = new DateTime($data->date_of_birth);
+        $datetime2 = new DateTime(date('Y-m-d'));
+        $interval = $datetime1->diff($datetime2);
+        $age =  $interval->format('%y');
+        ?>
         <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-        </tr><tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-        </tr><tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-        </tr><tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-        </tr><tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-        </tr><tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-        </tr><tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-        </tr><tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-        </tr><tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-        </tr><tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
+            <td align="center"><?php echo $counter; ?></td>
+            <td><?php echo $data->sewadar_name; ?></td>
+            <td><?php echo $data->father_dauther_son_wife_of; ?></td>
+            <td align="center"><?php echo ($data->gender == 'MALE') ? 'M' : 'F'; ?></td>
+            <td align="center"><?php echo $age; ?></td>
+            <td></td>
+            <td align="center"><?php echo $data->address1.' '.$data->address2.' '. $data->address3; ?></td>
+            <td align="center"><?php echo $data->mobile_primary; ?></td>
         </tr>
-        <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-        </tr><tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-        </tr><tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-        </tr
-      
-      
-    </table></td>
-  </tr>
+        <?php $counter++;
+    }
+    ?>
+
+</table></td>
+</tr>
 </table><br>
 <table width="682" cellpadding="0" cellspacing="0">
-  <tr>
-    <td>
-    <section style="float:left; padding:10px 0 5px 10px; ">
-    Name of Incharge : Narender Kumar<br>
-	     Badge No. :<br>
-	     Mobile No. : 9878904040<br>
-         Signature:
-         </section>
-  <section style="float:right; padding:10px 10px 5px 0;">
-	For Radha Soami Satsang Beas <br>
-    Secretary Baltana Centre<br>
-    Secretary/President<br>
-    Contact Nos. 98789-03823, 98558-24925
-    </section></td></tr>
-  </table>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p><br>
-  
-</p>
-<table width="682" cellpadding="0" cellspacing="0">
-  <tr><td>
-  
-<u><strong><center>For Area/Zonal Office Use</center></strong></u>
-<section style="float:left; padding:10px 0px 5px 10px;" >
-	Sr. No. : <?php echo $NominalRoll->serial_no; ?><br>
-	DATED: 16/08/2014<br>
-	No. Of Male Sewadars:<br>
-	Total Sewadars:
-</section>
-  <section style="float:right; padding:10px 10px 5px 0px;"  >
-    Area Secretary Contact Nos. 9779457500 <br>
-     Baltana Centre, Mohali Area, Sone- IB<br>
-     Department Name : Special Sewa<br>
-     Period From: 16/08/2014 to16/08/2014<br>
-<br>
-Incharge Signature
-  </section>
-
-  </td>
-  
-  </tr>
+    <tr>
+        <td>
+            <section style="float:left; padding:10px 0 5px 10px; ">
+                Name of Incharge : <?php echo $NominalRoll->sewadar_id; ?><br>
+                Badge No. : <?php echo $NominalRoll->incharge_badge_no; ?><br>
+                Mobile No. : <?php echo $NominalRoll->incharge_mobile_no; ?><br>
+                Signature:
+            </section>
+            <section style="float:right; padding:10px 10px 5px 0;">
+                <p>For Radha Soami Satsang Beas <br>
+                    Secretary Baltana Centre</p>
+                <p><br>
+                    Secretary/President<br>
+                    Contact Nos. <?php echo $NominalRoll->secretary_president_mobile_no; ?>
+                </p>
+            </section></td></tr>
 </table>
+
+<p><br></p>
+<table width="682" cellpadding="0" cellspacing="0">
+    <tr><td>
+
+            <u><strong><center>For Area/Zonal Office Use</center></strong></u>
+            <section style="float:left; padding:15px 0px 5px 10px;" >
+                Sr. No. : <?php echo $NominalRoll->serial_no; ?><br>
+                DATED: <?php echo date('d/m/Y' , strtotime($NominalRoll->dated)); ?><br>
+                No. Of Male Sewadars:<?php echo $NominalRoll->total_male; ?><br>
+                No. Of Female Sewadars:<?php echo $NominalRoll->total_female; ?><br>
+                Total Sewadars:<?php echo $NominalRoll->total_sewadar; ?>
+            </section>
+            <section style="float:right; padding:10px 10px 5px 0px;"  >
+                Area Secretary Contact Nos. <?php echo $NominalRoll->total_sewadar; ?> <br>
+                <?php echo $NominalRoll->centre_name; ?> Centre,  <?php echo $NominalRoll->area_name; ?> Area, Zone-  <?php echo $NominalRoll->zone_name; ?><br>
+                Department Name : <?php echo $NominalRoll->department_name; ?><br>
+                Period From: <?php echo date('d/m/Y', strtotime($NominalRoll->period_from)); ?> to <?php echo date('d/m/Y',strtotime($NominalRoll->period_to)); ?><br><br>
+                Incharge Signature
+            </section>
+
+        </td>
+
+    </tr>
+</table>
+<br>
+
 </div>
 </body>
 </html>
--- protected/views/nominalRoll/update.php
+++ protected/views/nominalRoll/update.php
@@ -6,41 +6,40 @@ $this->breadcrumbs=array(
 );
 
 echo $this->renderPartial('_update',array('model'=>$model));
-
+/*
 if(count($ListData)>0) { ?>
 
     <div class="black_overlay"></div>
     <div class="popup_wrapper p30">
-    <h3>Search Result</h3>
-    <table class="table">
-    <tr>
-        <th></th>
-        <th>Serial No</th>
-        <th>Badge No.</th>
-        <th>Name</th>
-        <th>Father/dauther/son/wife Name</th>
-        <th>Gender</th>
-        <th></th>
-    </tr>
-    <?php
-        foreach($ListData as $list) { ?>
+        <h3>Search Result</h3>
+        <table class="table">
             <tr>
-                <td><?php echo ($list->sewardar_picture!="") ? CHtml::image(Yii::app()->request->baseUrl."/attachments/sewadars_images/".$list->sewardar_picture, $list->sewardar_picture,array("width"=>100,"height"=>100)) : CHtml::image(Yii::app()->request->baseUrl."/attachments/sewadars_images/no-pic.png","no image uploaded",array("width"=>100,"height"=>100)); ?></td>
-                <td><?php echo $list->serial_no; ?></td>
-                <td><?php echo $list->badge_no; ?></td>
-                <td><?php echo $list->sewadar_name; ?></td>
-                <td><?php echo $list->father_dauther_son_wife_of; ?></td>
-                <td><?php echo $list->gender; ?></td>
-                <td><?php echo CHtml::link('+ Add',array('addSingleData','nominal_roll_id'=>$model->nominal_roll_id,'user_id'=>$list->sewadar_id),array('class' => 'btn'))?></td>
+                <th>Serial No</th>
+                <th>Badge No.</th>
+                <th>Name</th>
+                <th>Father/dauther/son/wife Name</th>
+                <th>Gender</th>
+                <th></th>
             </tr>
-        <?php
-        }
-    ?>
-    </table>
-</div>
+            <?php
+            foreach($ListData as $list) { ?>
+                <tr>
+                    <td><?php echo ($list->sewardar_picture!="") ? CHtml::image(Yii::app()->request->baseUrl."/attachments/sewadars_images/".$list->sewardar_picture, $list->sewardar_picture,array("width"=>100,"height"=>100)) : CHtml::image(Yii::app()->request->baseUrl."/attachments/sewadars_images/no-pic.png","no image uploaded",array("width"=>100,"height"=>100)); ?></td>
+                    <td><?php echo $list->serial_no; ?></td>
+                    <td><?php echo $list->badge_no; ?></td>
+                    <td><?php echo $list->sewadar_name; ?></td>
+                    <td><?php echo $list->father_dauther_son_wife_of; ?></td>
+                    <td><?php echo $list->gender; ?></td>
+                    <td><?php echo CHtml::link('+ Add',array('addSingleData','nominal_roll_id'=>$model->nominal_roll_id,'user_id'=>$list->sewadar_id),array('class' => 'btn'))?></td>
+                </tr>
+            <?php
+            }
+            ?>
+        </table>
+    </div>
 <?php
 }
-
+*/
 $this->beginWidget('bootstrap.widgets.TbModal', array('id'=>'myModal')); ?>
 
     <div class="modal-header">
@@ -55,9 +54,10 @@ $this->beginWidget('bootstrap.widgets.TbModal', array('id'=>'myModal')); ?>
 <?php
 $this->endWidget();
 ?>
+<hr/>
 <div class="container-fluid">
 <?php
-$this->widget('bootstrap.widgets.TbButton', array(
+/*$this->widget('bootstrap.widgets.TbButton', array(
     'label'=>'Add New Sewadar',
     'type'=>'primary',
     'htmlOptions'=>array(
@@ -65,50 +65,112 @@ $this->widget('bootstrap.widgets.TbButton', array(
         'data-target'=>'#myModal',
         'class'=>'span2',
     ),
-));
+));*/
 echo CHtml::link('Print',array('printSewa','id'=>$model->nominal_roll_id),array('class' => 'btn btn-primary span2'))
 ?>
 </div>
+<div class="span">
+    <div class="span10">
+        <h2>Search Sewadar</h2>
 
-
-<h2>Sewadar List</h2>
-<?php
-if (count($NominalRollUserList)>0) { ?>
-    <table class="table table-bordered table-condensed table-striped">
-        <thead>
+        <?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm',array(
+            'id' =>'search-sewadar',
+            'method'=>'get',
+        )); ?>
+        <table>
             <tr>
-                <th></th>
-                <th>Serial No</th>
-                <th>Name of Sewadar/Sewadarni</th>
-                <th>Father/Husband Name</th>
-                <th>Sex</th>
-                <th>Age</th>
-                <th>address</th>
-                <th>Contact No.</th>
-                <th></th>
-            </tr>
-        </thead>
+                <td><?php echo $form->textFieldRow($sewadarModel,'serial_no',array('class'=>'span1')); ?></td>
+                <td><?php echo $form->textFieldRow($sewadarModel,'badge_no',array('class'=>'span1')); ?></td>
+                <td><?php echo $form->textFieldRow($sewadarModel,'sewadar_name',array('class'=>'span2','maxlength'=>45)); ?></td>
+                <td><?php $this->widget('bootstrap.widgets.TbButton', array(
+                        'buttonType'=>'submit',
+                        'type'=>'primary',
+                        'label'=>'Search',
+                            'htmlOptions' =>array('style'=>'margin-top:12px;')
+                    )
 
-<?php
-    foreach($NominalRollUserList as $list) {
-        $i= 1;
-        $data = Sewadars::model()->findByPk($list->sewadar_id);
-        $datetime1 = new DateTime($data->date_of_birth);
-        $datetime2 = new DateTime(date('Y-m-d'));
-        $interval = $datetime1->diff($datetime2);
-        $age =  $interval->format('%y');
-?>
-        <tr>
-            <td><?php echo ($data->sewardar_picture!="") ? CHtml::image(Yii::app()->request->baseUrl."/attachments/sewadars_images/".$data->sewardar_picture, $data->sewardar_picture,array("width"=>100,"height"=>100)) : CHtml::image(Yii::app()->request->baseUrl."/attachments/sewadars_images/no-pic.png","no image uploaded",array("width"=>100,"height"=>100)); ?></td>
-            <td><?php echo $i; ?></td>
-            <td><?php echo $data->sewadar_name; ?></td>
-            <td><?php echo $data->father_dauther_son_wife_of; ?></td>
-            <td><?php echo $data->gender; ?></td>
-            <td><?php echo $age; ?></td>
-            <td><?php echo $data->address1.' '.$data->address2.' '. $data->address3; ?></td>
-            <td><?php echo $data->mobile_primary; ?></td>
-            <td><?php echo CHtml::link('- Remove',array('removeSingleData','id'=>$list->nominal_roll_detail_id),array('class' => 'btn'))?></td>
-        </tr>
-    <?php $i++; } ?>
-</table>
-<?php } ?>
+                    ); ?></td>
+            </tr>
+        </table>
+
+        <?php $this->endWidget();?>
+
+    </div>
+
+    <div class="clearfix"></div>
+
+    <div class="span7">
+        <h3>Search Result</h3>
+       <?php if (count($ListData)>0) { ?>
+            <div>
+
+                <table class="table">
+                    <tr>
+                        <th>Serial No</th>
+                        <th>Badge No.</th>
+                        <th>Name</th>
+                        <th>Father/Husband Name</th>
+                        <th>Gender</th>
+                        <th></th>
+                    </tr>
+                    <?php
+                    foreach($ListData as $list) { ?>
+                        <tr>
+                            <!--<td><?php //echo ($list->sewardar_picture!="") ? CHtml::image(Yii::app()->request->baseUrl."/attachments/sewadars_images/".$list->sewardar_picture, $list->sewardar_picture,array("width"=>100,"height"=>100)) : CHtml::image(Yii::app()->request->baseUrl."/attachments/sewadars_images/no-pic.png","no image uploaded",array("width"=>100,"height"=>100)); ?></td>-->
+                            <td><?php echo $list->serial_no; ?></td>
+                            <td><?php echo $list->badge_no; ?></td>
+                            <td><?php echo $list->sewadar_name; ?></td>
+                            <td><?php echo $list->father_dauther_son_wife_of; ?></td>
+                            <td><?php echo $list->gender; ?></td>
+                            <td><?php echo CHtml::link('+ Add',array('addSingleData','nominal_roll_id'=>$model->nominal_roll_id,'user_id'=>$list->sewadar_id),array('class' => 'btn'))?></td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                </table>
+            </div>
+        <?php
+        }
+        ?>
+    </div>
+    <div class="span9">
+
+    <h2>Sewadar List</h2>
+    <?php
+    if (count($NominalRollUserList)>0) { ?>
+        <table class="table table-bordered table-condensed table-striped">
+            <thead>
+                <tr>
+                    <th>Serial No</th>
+                    <th>Name of Sewadar/Sewadarni</th>
+                    <th>Father/Husband Name</th>
+                    <th>Contact No.</th>
+                    <th>Address</th>
+                    <th></th>
+                </tr>
+            </thead>
+
+    <?php
+    $i= 1;
+        foreach($NominalRollUserList as $list) {
+
+            $data = Sewadars::model()->findByPk($list->sewadar_id);
+            $datetime1 = new DateTime($data->date_of_birth);
+            $datetime2 = new DateTime(date('Y-m-d'));
+            $interval = $datetime1->diff($datetime2);
+            $age =  $interval->format('%y');
+    ?>
+            <tr>
+
+                <td><?php echo $i; ?></td>
+                <td><?php echo $data->sewadar_name; ?></td>
+                <td><?php echo $data->father_dauther_son_wife_of; ?></td>
+                <td><?php echo $data->mobile_primary; ?></td>
+                <td><?php echo $data->address1.' '.$data->address2.' '.$data->address3; ?></td>
+                <td><?php echo CHtml::link('- Remove',array('removeSingleData','id'=>$list->nominal_roll_detail_id),array('class' => 'btn'))?></td>
+            </tr>
+        <?php $i++; } ?>
+    </table>
+    <?php } ?>
+        </div>
+</div>
